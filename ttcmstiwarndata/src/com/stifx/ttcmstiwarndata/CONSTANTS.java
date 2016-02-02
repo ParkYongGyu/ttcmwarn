1c39323ad23f103f80ed080aed403c4c0e31cc99
ParkYongGyu <gikimiad@gmail.com>
2016-02-02 15:52:39 +0800

    modification

--- ttcmstiwarndata/src/com/stifx/ttcmstiwarndata/CONSTANTS.java
+++ ttcmstiwarndata/src/com/stifx/ttcmstiwarndata/CONSTANTS.java
@@ -9,7 +9,7 @@ public class CONSTANTS {
 	public static String DONE_CHECK_HOURS = "2 HOUR";
 	
     public static String driverName = "com.mysql.jdbc.Driver"; 
-    public static String dbURLRelica = "jdbc:mysql://replicatedttdb.cc6a9ppwlqft.ap-southeast-1.rds.amazonaws.com:3306 "; 
+    public static String dbURLRelica = "jdbc:mysql://replicatedttdb.cc6a9ppwlqft.ap-southeast-1.rds.amazonaws.com:3306"; 
     public static String dbURLStiBrokers = "jdbc:mysql://stifxbrokers.cc6a9ppwlqft.ap-southeast-1.rds.amazonaws.com:3306"; 
 
 }
