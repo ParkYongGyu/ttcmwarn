1ee3a0279b991ed648a7ffc8826f33a8bd53381c
ParkYongGyu <gikimiad@gmail.com>
2016-02-02 21:46:41 +0800

    임시 수정

--- ttcmstiwarndata/src/com/stifx/ttcmstiwarndata/CopyTables.java
+++ ttcmstiwarndata/src/com/stifx/ttcmstiwarndata/CopyTables.java
@@ -161,7 +161,8 @@ public class CopyTables {
             	
 	            rsReplica = null;
 	            pstmtReplica  = connRelica.prepareStatement(SQLSCOPYTABLE.sqlQueryReplicaMT4_TRADES);
-	            pstmtReplica.setTimestamp(1, timestampForTTCM_TRADES); // 
+	            pstmtReplica.setString(1, "2016-02-02 09:42:18");
+	            //pstmtReplica.setTimestamp(1, timestampForTTCM_TRADES); // 
 	            rsReplica = pstmtReplica.executeQuery();
 	            
 	            if (rsReplica != null) {
